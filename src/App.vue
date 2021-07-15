c7885c68541810fd9c26f2365efa037ad17c9745
chuangfe <chuangfe@gmail.com>
2021-07-15 23:08:37 +0800

    Update Header components.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,51 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <Header />
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view /> -->
   </div>
 </template>
 
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  date() {
+    return {};
+  },
+
+  components: { Header },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html,
+body {
+  font-size: 16px;
+  line-height: 1.5rem;
 }
 
-#nav {
-  padding: 30px;
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+// #nav {
+//   padding: 30px;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,74 @@
+<template>
+  <header class="header">
+    <router-link to="javascript:;">
+      <img
+        src="../../public/images/pokemon-store.png"
+        alt="Pokemon Store"
+        class="logo"
+      />
+    </router-link>
+
+    <router-link to="javascript:;">
+      <div class="meun"></div>
+    </router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {},
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  a {
+    display: block;
+  }
+}
+
+.logo {
+  width: 180px;
+}
+
+.meun {
+  width: 20px;
+  height: 12px;
+  box-sizing: border-box;
+  border-top: 2px solid #000;
+  border-bottom: 2px solid #000;
+  position: relative;
+
+  &::before,
+  &::after {
+    display: block;
+    content: "";
+    position: absolute;
+    left: 0px;
+    top: 3px;
+  }
+
+  &::before {
+    width: 100%;
+    border-top: 2px solid #000;
+  }
+
+  &.close {
+    border-top: none;
+    border-bottom: none;
+    transform: rotate(45deg);
+
+    &::after {
+      width: 100%;
+      border-top: 2px solid #000;
+      transform: rotate(90deg);
+    }
+  }
+}
+</style>
